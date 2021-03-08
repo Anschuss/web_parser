e3ecbf2b924c8809f0c5957adea49eb8d07dfcb7
alexey <cuteracoon@gmail.com>
2021-03-08 13:16:22 +0600

    found problems with the bbc parser

--- views/parsers/run_pars.py
+++ views/parsers/run_pars.py
@@ -1,6 +1,5 @@
 from app import client
 
-# from .bbc.bbc_parser import bbc
 from .dw.dw_parser import dw
 from .nplus.n_plus_parser import n_plus
 from config import URL_PLUS_ONE, URL_DW
@@ -8,6 +7,5 @@ from config import URL_PLUS_ONE, URL_DW
 
 @client.task
 def run():
-    # bbc(URL_BBC)
     dw(URL_DW)
     n_plus(URL_PLUS_ONE)
